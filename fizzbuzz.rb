6b45d8ed37d1161f0ce2af142e58da00fc493f9c
Drew Thompson <drewallynthompson@gmail.com>
2020-10-30 22:37:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+<<<<<<< HEAD
 
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
@@ -12,4 +13,8 @@ def fizzbuzz(number)
   else 
    puts number
   end
+=======
+def fizzbuzz(int)
+  
+>>>>>>> 297abc5872449b459c829f8c5bbe70583f5e4150
 end
